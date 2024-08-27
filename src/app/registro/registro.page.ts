7a470ee979a9f70dd9b22c4d4620350f235d9dc1
Dayana Hoyos Riascos <da.hoyos@duocuc.cl>
2024-08-27 17:53:12 -0400

    add register view

---
+++ src/app/registro/registro.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.page.html',
+  styleUrls: ['./registro.page.scss'],
+})
+export class RegistroPage implements OnInit {
+
+  registerForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    rut: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
+
+}
