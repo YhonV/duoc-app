b7204258ae2650dfa43b78726189d00294e1e72b
Dayana Hoyos Riascos <da.hoyos@duocuc.cl>
2024-09-01 17:36:44 -0400

    actualizo home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -6,34 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  currentDate: string;
+ 
+  constructor() {}
 
-  constructor() {
-    this.currentDate = this.getFormattedDate();
-  }
-
-  getFormattedDate(): string {
-    const date = new Date();
-    const options: Intl.DateTimeFormatOptions = {
-      weekday: 'long',
-      day: '2-digit',
-      month: 'long',
-      year: 'numeric'
-    };
-    
-    let formattedDate = date.toLocaleDateString('es-ES', options);
-  
-    // Divide la cadena en palabras
-    let words = formattedDate.split(' ');
-    
-    // Capitaliza la primera palabra (día de la semana) y la tercera palabra (mes)
-    words[0] = words[0].charAt(0).toUpperCase() + words[0].slice(1);
-    words[3] = words[3].charAt(0).toUpperCase() + words[3].slice(1);
-    
-    // Une las palabras de nuevo en una cadena
-    return words.join(' ');
-    
-  }
+ 
 
   ngOnInit() {
   }
