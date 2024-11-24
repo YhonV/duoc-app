bec2f4f735ab97c8605a552f87a044b9e9e10ee0
Dayana Hoyos Riascos <da.hoyos@duocuc.cl>
2024-11-24 13:32:52 -0300

    asistencia prueba

--- src/app/pages/assistance-student/assistance-student.page.spec.ts
+++ src/app/pages/assistance-student/assistance-student.page.spec.ts
@@ -1,11 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AssistanceStudentPage } from './assistance-student.page';
+import { Preferences } from '@capacitor/preferences';
+import { UtilService } from 'src/app/services/utils.service';
+import { BarcodeScanner } from 'capacitor-barcode-scanner';
+import { IonicModule } from '@ionic/angular';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment.prod';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AssistanceStudentPage', () => {
   let component: AssistanceStudentPage;
   let fixture: ComponentFixture<AssistanceStudentPage>;
 
-  beforeEach(() => {
+  beforeEach( async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AssistanceStudentPage ],
+      imports: [IonicModule.forRoot(),
+                AngularFireModule.initializeApp(environment.firebaseConfig),
+                HttpClientModule
+      ],
+      providers: [ UtilService, Preferences, BarcodeScanner ]
+    }).compileComponents();
+
+
     fixture = TestBed.createComponent(AssistanceStudentPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
