f3f6617a163b1be94ca438e9a8c96d5ee10b5bb5
Dayana Hoyos Riascos <da.hoyos@duocuc.cl>
2024-11-24 14:30:51 -0300

    test login

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -9,6 +9,7 @@ import { Preferences } from '@capacitor/preferences';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment.prod';
 import { HttpClientModule } from '@angular/common/http';
+import { UserCredential } from 'firebase/auth';
 
 describe('LoginPage', () => {
   let component: LoginPage;
@@ -33,4 +34,29 @@ describe('LoginPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Funciona  OnSubmit', async () => {
+    const email = 'dahori.10@gmail.com';
+    const password = '123456';
+    component.form.controls['email'].setValue(email);
+    component.form.controls['password'].setValue(password);
+    expect(component.form.valid).toBeTruthy();
+
+
+    const firebaseServiceSpy = spyOn(TestBed.inject(FirebaseService), 'signIn').and.returnValue(
+      Promise.resolve({
+        user: { 
+          uid: '123',
+          email: email,
+          displayName: null, // O el valor correspondiente si existe
+          phoneNumber: null, // O el valor correspondiente si existe
+          photoURL: null,    // O el valor correspondiente si existe
+          providerId: 'firebase',
+          // Incluye aquí cualquier otra propiedad necesaria para tu caso
+        },
+        providerId: 'firebase',
+        operationType: 'signIn', // Puede ser 'signIn' o lo que corresponda
+      } as UserCredential) // Esto asegura que TypeScript lo trate como un `UserCredential`
+    );
+    });
 });
