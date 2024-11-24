81764ac6246f4aa5b6a37a26d10a22aa4828b540
Dayana Hoyos Riascos <da.hoyos@duocuc.cl>
2024-11-24 13:18:37 -0300

    perfil prueba

--- src/app/pages/profile/profile.page.spec.ts
+++ src/app/pages/profile/profile.page.spec.ts
@@ -1,11 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProfilePage } from './profile.page';
+import { Preferences } from '@capacitor/preferences';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilService } from 'src/app/services/utils.service';
+import { IonicModule } from '@ionic/angular';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment.prod';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ProfilePage', () => {
   let component: ProfilePage;
   let fixture: ComponentFixture<ProfilePage>;
 
-  beforeEach(() => {
+  beforeEach( async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProfilePage ],
+      imports: [IonicModule.forRoot(),
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        HttpClientModule
+      ],
+      providers: [ FirebaseService, UtilService, Preferences]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ProfilePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
